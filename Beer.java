f7aee2f14fd172e44e575fd69a19a00299170200
quandg_57 <quandg_57>
2014-07-03 20:54:53 +0700

    asg01

---
+++ Beer.java
@@ -0,0 +1,13 @@
+public class Beer{
+  public static void main ( String[] args ){ 
+     System.out.println("Do Gia Quan \t K57CCLC \t12020309 \t quandg_57 \t ");
+	 for(int i=9;i>0;i--)
+	 {
+	    System.out.println( i + " bottles of beer on the wall, " + i + " bottles of beer.");
+		System.out.println("Take one down, pass it around,");
+	 }
+	 System.out.println("Take one down, pass it around,");
+	 System.out.println("No more bottles of beer on the wall.");
+	
+	}
+}
